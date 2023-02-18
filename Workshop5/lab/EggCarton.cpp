f91d64538509aa5085346f4a09a29cc304ac91b0
Marcus Brown <marcus.brown1234567@gmail.com>
2023-02-17 23:34:44 -0500

    few small changes should be working

--- Workshop5/lab/EggCarton.cpp
+++ Workshop5/lab/EggCarton.cpp
@@ -1,11 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <cstring>
 #include "EggCarton.h"
 using namespace std;
 namespace sdds
 {
-    EggCarton::EggCarton(int size = 6, int noOfEggs = 0, bool jumboSize = false)
+    EggCarton::EggCarton(int size, int noOfEggs, bool jumboSize)
     {
         if (size % 6 != 0 || (size < 6 || size > 36) || (noOfEggs < 0 || noOfEggs > size))
         {
@@ -74,20 +73,20 @@ namespace sdds
     }
     EggCarton::operator int() const
     {
-        return m_noOfEggs > 0 ? m_noOfEggs : -1;
+        return bool(*this) ? m_noOfEggs : -1;
     }
     EggCarton::operator double() const
     {
-        double total;
+        double total = 0.0;
         if (bool(*this))
         {
-            if (m_jumboSize == true)
+            if (m_jumboSize)
             {
-                total = (m_noOfEggs * JumboEggWieght) / 1000;
+                total = (m_noOfEggs * JumboEggWieght) / 1000.0;
             }
             else
             {
-                total = (m_noOfEggs * RegularEggWieght) / 1000;
+                total = (m_noOfEggs * RegularEggWieght) / 1000.0;
             }
             return total;
         }
@@ -134,33 +133,31 @@ namespace sdds
         }
         return *this;
     }
+    // EggCarton &EggCarton::operator=(EggCarton &right)
+    // {
+    //     m_size = right.m_size;
+    //     m_noOfEggs = right.m_noOfEggs;
+    //     m_jumboSize = right.m_jumboSize;
 
-    EggCarton &EggCarton::operator=(EggCarton &right)
-    {
-        m_size = right.m_size;
-        m_noOfEggs = right.m_noOfEggs;
-        m_jumboSize = right.m_jumboSize;
-
-        return *this;
-    }
-
+    //     return *this;
+    // }
     EggCarton &EggCarton::operator+=(int value)
     {
         if (bool(*this))
         {
             m_noOfEggs += value;
-        }
-        if (m_noOfEggs > m_size)
-        {
-            setBroken(-1, -1);
+            if (m_noOfEggs > m_size)
+            {
+                setBroken(-1, -1);
+            }
         }
         return *this;
     }
-    EggCarton &EggCarton::operator+=(EggCarton &right)
+    EggCarton &EggCarton::operator+=(EggCarton &right) // maybe fix this too
     {
-        int leftSpots = m_size - m_noOfEggs;
         if (bool(*this))
         {
+            int leftSpots = m_size - m_noOfEggs;
             while (leftSpots != 0 && right.m_noOfEggs != 0)
             {
                 leftSpots--;
@@ -172,22 +169,25 @@ namespace sdds
     }
     bool EggCarton::operator==(const EggCarton &right) const // fix this function
     {
-        double difference = double(*this) - double(right);
-        if (difference >= -0.001 || difference <= 0.001)
+        double difference = double(m_jumboSize) - double(right.m_jumboSize);
+        if (difference >= -0.001 && difference <= 0.001)
         {
-            return true;
+            return false;
         }
-        return false;
+        return true;
     }
     int operator+(int left, const EggCarton &right)
     {
+        int sum = 0;
         if (bool(right))
         {
-            return left + right;
+            sum = left + right;
+            return sum;
         }
         else
         {
-            return left;
+            sum = left;
+            return sum;
         }
     }
     ostream &operator<<(ostream &ostr, const EggCarton &right)
@@ -199,5 +199,8 @@ namespace sdds
     {
         right.read(istr);
         return istr;
+    }
+    EggCarton::~EggCarton()
+    {
     }
 }
